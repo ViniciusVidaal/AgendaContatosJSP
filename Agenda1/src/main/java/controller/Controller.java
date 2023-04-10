8a4edfcd7344e9b3fc38e502372d2c51ed5f105e
Vinicius Vidal <viniciusvidal061@gmail.com>
2023-04-10 15:54:59 -0300

    setando as variaveis do javaBeans

--- Agenda1/src/main/java/controller/Controller.java
+++ Agenda1/src/main/java/controller/Controller.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
-
+import model.JavaBeans;
 
 @WebServlet(urlPatterns= {"/Controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
-       
+    JavaBeans contato = new JavaBeans();
 public Controller() {
         super();
     }
@@ -47,6 +47,12 @@ public Controller() {
 		System.out.println(request.getParameter("nome"));
 		System.out.println(request.getParameter("fone"));
 		System.out.println(request.getParameter("email"));
+		//setar as variaveis javabeans
+		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
 	}
 
 }
