f20ba968ae73baac5fc105a4017c2b2914f4dc6b
Vinicius Vidal <viniciusvidal061@gmail.com>
2023-04-12 15:45:21 -0300

    Armazanei dados do banco no array e mechi no Style da tabela agenda.

--- Agenda1/src/main/java/controller/Controller.java
+++ Agenda1/src/main/java/controller/Controller.java
@@ -1,65 +1,90 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import model.DAO;
 import model.JavaBeans;
 
-@WebServlet(urlPatterns= {"/Controller", "/main", "/insert"})
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
-    JavaBeans contato = new JavaBeans();
-public Controller() {
-        super();
-    }
+	JavaBeans contato = new JavaBeans();
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Controller() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getServletPath();
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
 		} else if (action.equals("/insert")) {
 			novoContato(request, response);
-			
+
 		} else {
 			response.sendRedirect("agenda.jsp");
 		}
-		
-		
+
 		// Teste de conexao
-		//dao.testeConexao();
+		// dao.testeConexao();
 	}
-	
-	//listar Contatos
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
-	}
-	//novoContato
-	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	// listar Contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// criando um objeto que irá receber os dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
 		
-		/*teste de recebimento dos dados do formulario
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("fone"));
-		System.out.println(request.getParameter("email"));
+		//encaminhar a lista ao documento agenda.jsp
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+		
+		
+		
+		/*
+		 * teste para ver se a lista esta recebendo os contatos normalmente for(int i =
+		 * 0; i < lista.size(); i++) { System.out.println(lista.get(i).getIdcon());
+		 * System.out.println(lista.get(i).getNome());
+		 * System.out.println(lista.get(i).getFone());
+		 * System.out.println(lista.get(i).getEmail()); }
+		 * 
 		*/
-		
-		//setar as variaveis javabeans
-		
+	}
+
+	// novoContato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		/*
+		 * teste de recebimento dos dados do formulario
+		 * System.out.println(request.getParameter("nome"));
+		 * System.out.println(request.getParameter("fone"));
+		 * System.out.println(request.getParameter("email"));
+		 */
+
+		// setar as variaveis javabeans
+
 		contato.setNome(request.getParameter("nome"));
 		contato.setFone(request.getParameter("fone"));
 		contato.setEmail(request.getParameter("email"));
-		
+
 		// invocar o metodo inserirContato passando o objeto contato
-		dao.InserirContatos(contato);	
+		dao.InserirContatos(contato);
 		// redirecionando para o documento agenda.jsp
 		response.sendRedirect("main");
-		
+
 	}
 
 }
